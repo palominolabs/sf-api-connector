12c10c7488cda1307e170aac562b71498bd63243
Marshall Pierce <marshall@palominolabs.com>
2013-05-14 12:21:33 -0700

    Update copyright

--- rest-api-connector/src/main/java/com/palominolabs/crm/sf/rest/BasicSObjectUrls.java
+++ rest-api-connector/src/main/java/com/palominolabs/crm/sf/rest/BasicSObjectUrls.java
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2011. Team Lazer Beez (http://teamlazerbeez.com)
+ * Copyright © 2013. Palomino Labs (http://palominolabs.com)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
